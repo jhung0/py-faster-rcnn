717b6cba4948192d3aecdca30593ecf43895db0d
Ubuntu <ubuntu@ip-10-0-0-74.ec2.internal>
2017-02-11 05:33:34 +0000

    run2stage path

--- run2stage.py
+++ run2stage.py
@@ -5,7 +5,7 @@ def add_path(path):
     #if path not in sys.path:
         sys.path.insert(0, path)
 
-this_dir = '/py-faster-rcnn/' #osp.dirname(__file__)
+this_dir = '/home/ubuntu/py-faster-rcnn/' #osp.dirname(__file__)
 caffe_path = os.path.join(this_dir,  'caffe-fast-rcnn', 'python')
 add_path(caffe_path)
 lib_path = os.path.join(this_dir, 'lib')
