93e642ff176ddc8fb2af3542d96ea4e5f3437bc2
Ubuntu <ubuntu@ip-10-0-0-74.ec2.internal>
2017-02-13 19:06:43 +0000

    try to upload image

--- web/app.py
+++ web/app.py
@@ -1,9 +1,28 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request
+from flask_uploads import UploadSet, IMAGES, configure_uploads
+import os
+app = Flask(__name__, instance_path='/home/ubuntu/py-faster-rcnn/web/instance')
+#app.config.from_object('app.default_settings')
+#app.config.from_pyfile('app.cfg', silent=True) 
+
+# Configure the image uploading via Flask-Uploads
+images = UploadSet('images', IMAGES)
+app.config['UPLOADED_PHOTOS_DEST'] = '/home/ubuntu/images'
+configure_uploads(app, images)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST' and 'image' in request.files:
+        filename = images.save(request.files['image'])
+        rec = Image(filename=filename)
+        rec.store()
+        return redirect(url_for('show', id=rec.id))
+    return render_template('upload.html')
 
 @app.route('/')
-def hello_world():
+def showResult():
     return 'Flask Dockerized'
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
